a016e33ce0fa5ae80f635436a8726be87c014f57
jgvero32 <jgvero32@gmail.com>
2024-10-03 22:36:29 -0500

    Add initial page and icon

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>
+          Jelena is in the process of developing this website. 
+        </h1>
+        <p>
+          Please check back soon.
+          </p>
+    </div>
+  );
+}
+
+export default App;
